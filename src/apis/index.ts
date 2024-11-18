10790d683c3740e236d2058349cfe667e97779d3
Yoo TaeSeung <remicon99@gmail.com>
2024-11-18 19:32:05 +0900

    hotfix: api 요청시간 수정 1분 -> 10분

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_APP_BASE_URL,
-  timeout: 60000,
+  timeout: 600000,
 });
 
 export function get<T>(...args: Parameters<typeof instance.get>) {
